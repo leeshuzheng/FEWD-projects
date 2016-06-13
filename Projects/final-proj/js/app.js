3867fbeea975c31657cd56129abab90758c8c580
Hadi <leeshuzheng@gmail.com>
2016-06-13 09:52:27 +0800

    Final Project

--- Projects/final-proj/js/app.js
+++ Projects/final-proj/js/app.js
@@ -3,14 +3,23 @@ $(document).ready(function() {
   // $('').click(function(event) {
   // event.preventDefault();
   // });
-$('#myBlock').vide('video/mujivideo');
-$('#rightAtHome').vide('video/rightathome');
+$('#myBlock').vide('../video/mujivideo');
+$('#rightAtHome').vide('../video/rightathome');
 
 
 $('#fullpage').fullpage({
-  anchors:['page1', 'page2', 'page3', 'page4'],
-  autoScrolling: false;
+  // anchors:['page1', 'page2', 'page3', 'page4', 'page5'],
+  autoScrolling: false,
+  normalScrollElements: ['#campaignContainer, .informationContainer'],
+  fitToSection: false
 });
 
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.animate', {duration: 600 });
+
+$('#rightAtHome').resize(function() {
+  console.log('resizing!');
+})
 
 });
